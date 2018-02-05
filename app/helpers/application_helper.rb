dfcdc4cf697afe166e859d48c823f13f8e1bbccf
Utah3002 <peterson.spence@gmail.com>
2018-02-05 21:21:24 +0000

    Updated the look of the Tech news page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,6 +39,10 @@ module ApplicationHelper
         title: 'About Me'
       },
       {
+        url: tech_news_path,
+        title: 'Tech News'
+      },
+      {
         url: contact_path,
         title: 'Contact Me'
       },
